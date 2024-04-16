9932227827bb6f04d9aa37aefee52ce4ca7bbce0
AbdiIbrahimAdan <abdishu98@gmail.com>
2024-04-16 22:14:57 +0300

    successfully completed

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module containing the function from_json_string"""
+import json
+
+
+def from_json_string(my_str):
+    """Returns an object (Python data structure) represented by a JSON string.
+
+    Args:
+        my_str (str): json object to convert to Python object.
+
+    Returns:
+        type: Python object.
+    """
+    return json.loads(my_str)
---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module containing the function save_to_json_file"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Writes an Object to a text file using JSON representation.
+
+    Args:
+        my_obj (type): Object to write to text file.
+        filename (str): name of the file.
+
+    Returns:
+        type: JSON representation.
+    """
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        json_object = json.dumps(my_obj)
+        f.write(json_object)
+        f.close()
