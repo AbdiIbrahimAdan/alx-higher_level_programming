0eaee1fc92a9e3a565326852aa11840da37dcc40
AbdiIbrahimAdan <abdishu98@gmail.com>
2024-05-23 02:50:16 -0700

    full creation - create table and add data into it

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "George", 8);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
 
